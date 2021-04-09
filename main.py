9da85c218519c03795b18e75951e3baf690d9c9f
princesinghtomar <singhtomarprince470@gmail.com>
2021-04-09 15:12:10 +0530

    Added New Files

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import cvxpy as cp
 import sys
 import os
 
-MINM = -1000000000
+MINM = -100000000
 
 #Hyperparameters
 DELTA = 0.001
-GAMMA = 0.999         
-NOISE = 0  
+GAMMA = 0.999
 
 #Define all pos
 all_pos=[]
-all_pos.append('W')
-all_pos.append('N')
-all_pos.append('E')
-all_pos.append('S')
 all_pos.append('C')
+all_pos.append('N')
+all_pos.append('S')
+all_pos.append('E')
+all_pos.append('W')
 
 #Define all state
 all_states=[]
@@ -25,22 +24,22 @@ all_states.append('R')
 
 max_mat = 3
 max_arrow = 4
-E_left = 0
 max_health = 5
+STEP_COST = -20
 total_states = 600
 
 #Reward for each action
 action_reward = {
-    'UP': -10,
-    'LEFT': -10,
-    'DOWN': -10,
-    'RIGHT': -10,
-    'STAY': -10,
-    'SHOOT': -10,
-    'HIT': -10,
-    'CRAFT': -10,
-    'GATHER': -10,
-    'NONE': 0
+    'UP': STEP_COST,
+    'LEFT': STEP_COST,
+    'DOWN': STEP_COST,
+    'RIGHT': STEP_COST,
+    'STAY': STEP_COST,
+    'SHOOT': STEP_COST,
+    'HIT': STEP_COST,
+    'CRAFT': STEP_COST,
+    'GATHER': STEP_COST,
+    'NONE': STEP_COST
 }
 
 #Define rewards for all states
@@ -57,13 +56,13 @@ actions = {
     'C':('UP', 'DOWN', 'LEFT', 'RIGHT', 'STAY', 'SHOOT', 'HIT')
     }
 
-#Dictionary for integer of pos of IJ
+#Dictionary for integer mapping of pos of IJ
 pos_map = {
-    'W' : 0,
+    'C' : 0,
     'N' : 1,
-    'E' : 2,
-    'S' : 3,
-    'C' : 4
+    'S' : 2,
+    'E' : 3,
+    'W' : 4
 }
 
 #Dictionary for integer mapping of states of MM
@@ -86,7 +85,6 @@ action_map = {
     'NONE': 9
 }
 
-#Final dictionary to be converted to json and submitted
 final_dict = {
     "a"         : "Empty",
     "r"         : "Empty",
@@ -135,184 +133,255 @@ def numtotuple(num):
     return (p,m,a,s,h)
 
 def prob(a,p,m,arr,s,h,p1,m1,arr1,s1,h1):
+    p_MM = 0
+    var = 0
+    if s == 'D' and s1 == 'R':
+        p_MM = 0.2
+    elif s == 'D' and s1 == 'D':
+        p_MM = 0.8
+    elif s == 'R' and s1 == 'R':
+        p_MM = 0.5
+    elif s == 'R' and s1 == 'D':
+        p_MM = 0.5
+        var = 1
+    
+    if var == 1 and (p == 'C' or p == 'E'):
+        if arr1!=0 or m!=m1:
+            return 0
+        elif (h+1)!=h1 and (h!=4 or h1!=4):
+            return 0
+        # (h+1)==h1 or (h==4 and h1==4)
+        else:
+            var = 2
     if a == 'UP':
-        # if m!=m1 or arr!=arr1 or h!=h1:
-        #     return 0
+        if m!=m1 or (var!=2 and arr!=arr1) or (var!=2 and h!=h1):
+            return 0
         if p == 'S':
             if p1 == 'C':
-                return probability[p]
+                return probability[p]*p_MM
             elif p1 == fail[p]:
-                return 1-probability[p]
+                return (1-probability[p])*p_MM
             else:
                 return 0
         elif p == 'C':
+            if var == 2:
+                if p1 == p:
+                    return p_MM
+                else:
+                    return 0
             if p1 == 'N':
-                return probability[p]
+                return probability[p]*p_MM
             elif p1 == fail[p]:
-                return 1-probability[p]
+                return (1-probability[p])*p_MM
             else:
                 return 0
         else:
             return 0
     elif a == 'DOWN':
-        # if m!=m1 or arr!=arr1 or s!=s1 or h!=h1:
-        #     return 0
+        if m!=m1 or (var!=2 and arr!=arr1) or (var!=2 and h!=h1):
+            return 0
         if p == 'N':
             if p1 == 'C':
-                return probability[p]
+                return probability[p]*p_MM
             elif p1 == fail[p]:
-                return 1-probability[p]
+                return (1-probability[p])*p_MM
             else:
                 return 0
         elif p == 'C':
+            if var == 2:
+                if p1 == p:
+                    return p_MM
+                else:
+                    return 0
             if p1 == 'S':
-                return probability[p]
+                return probability[p]*p_MM
             elif p1 == fail[p]:
-                return 1-probability[p]
+                return (1-probability[p])*p_MM
             else:
                 return 0
         else:
             return 0
     elif a == 'LEFT':
-        # if m!=m1 or arr!=arr1 or s!=s1 or h!=h1:
-        #     return 0
+        if m!=m1 or (var!=2 and arr!=arr1) or (var!=2 and h!=h1):
+            return 0
         if p == 'E':
-            if p1 == 'C' or (p1 == 'W' and E_left == 1):
-                return probability[p]
+            if var == 2:
+                if p1 == p:
+                    return p_MM
+                else:
+                    return 0
+            if (p1 == 'C'):
+                return probability[p]*p_MM
             elif p1 == fail[p]:
-                return 1-probability[p]
+                return (1-probability[p])*p_MM
             else:
                 return 0
         elif p == 'C':
+            if var == 2:
+                if p1 == p:
+                    return p_MM
+                else:
+                    return 0
             if p1 == 'W':
-                return probability[p]
+                return probability[p]*p_MM
             elif p1 == fail[p]:
-                return 1-probability[p]
+                return (1-probability[p])*p_MM
             else:
                 return 0
         else:
             return 0
     elif a == 'RIGHT':
-        # if m!=m1 or arr!=arr1 or s!=s1 or h!=h1:
-        #     return 0
+        if m!=m1 or (var!=2 and arr!=arr1) or (var!=2 and h!=h1):
+            return 0
         if p == 'W':
             if p1 == 'C':
-                return probability[p]
+                return probability[p]*p_MM
             elif p1 == fail[p]:
-                return 1-probability[p]
+                return (1-probability[p])*p_MM
             else:
                 return 0
         elif p == 'C':
+            if var == 2:
+                if p1 == p:
+                    return p_MM
+                else:
+                    return 0
             if p1 == 'E':
-                return probability[p]
-            elif p1 == fail[p]:
-                return 1-probability[p]
+                return 1*p_MM
             else:
                 return 0
         else:
             return 0
     elif a == 'STAY':
-        # if m!=m1 or arr!=arr1 or s!=s1 or h!=h1:
-        #     return 0
+        if m!=m1 or (var!=2 and arr!=arr1) or (var!=2 and h!=h1):
+            return 0
+        if var == 2 and (p == 'C' or p == 'E'):
+                if p1 == p:
+                    return p_MM
+                else:
+                    return 0
         if p1 == p:
-            return probability[p]
+#             print('reached 1')
+            return probability[p]*p_MM
         elif p1 == fail[p]:
-            return 1-probability[p]
+#             print('reached 2')
+            return (1-probability[p])*p_MM
         else:
             return 0
         
     elif a == 'SHOOT':
-        # if p!=p1 or m!=m1 or s!=s1:
-        #     return 0
-        if p!=p1 or m != m1:
+        if p!=p1 or m!=m1:
             return 0
+        if var == 2 and (p == 'C' or p == 'E'):
+            if p == 'C':
+                return p_MM
+            elif p == 'E':
+                return p_MM
         if arr == arr1+1 and (p == 'C' or p == 'E' or p == 'W'):
             if p == 'C':
                 if h == h1+1:
-                    return 0.5
+                    return 0.5*p_MM
                 elif h == h1:
-                    return 0.5
+                    return 0.5*p_MM
                 else:
                     return 0
             elif p == 'E':
                 if h == h1+1:
-                    return 0.9
+                    return 0.9*p_MM
                 elif h == h1:
-                    return 0.1
+                    return 0.1*p_MM
                 else:
                     return 0
             elif p == 'W':
                 if h == h1+1:
-                    return 0.25
+                    return 0.25*p_MM
                 elif h == h1:
-                    return 0.75
+                    return 0.75*p_MM
                 else:
                     return 0
         else:
             return 0
         
     elif a == 'HIT':
-        # if p!=p1 or m!=m1 or s!=s1 or arr!=arr1:
-        #     return 0
-        if p!=p1 or m!=m1:
+        if p!=p1 or m!=m1 or (var!=2 and arr!=arr1):
             return 0
+        if var == 2 and (p == 'C' or p == 'E'):
+            if p == 'C':
+                return p_MM
+            elif p == 'E':
+                return p_MM
         if p == 'C':
-            if h == h1+2:
-                return 0.1
+            if h == h1+2 or (h == h1+1 and h1 == 0):
+                return 0.1*p_MM
             elif h == h1:
-                return 0.9
+                return 0.9*p_MM
             else:
                 return 0
         elif p == 'E':
-            if h == h1+2:
-                return 0.2
+            if h == h1+2 or (h == h1+1 and h1 == 0):
+                return 0.2*p_MM
             elif h == h1:
-                return 0.8
+                return 0.8*p_MM
             else:
                 return 0
         else:
             return 0
         
     elif a == 'CRAFT':
-        # if p!=p1 or s!=s1 or h!=h1:
-        #     return 0
-        if p!=p1 or h!=h1:
+        if p!=p1 or h!=h1 or m==0:
             return 0
         if p == 'N' and m>=1 and m == m1+1:
-            if arr+1 == arr1:
-                return 0.5
-            elif arr+2 == arr1:
-                return 0.35
-            elif arr+3 == arr1:
-                return 0.15
-            else:
-                return 0
+            if arr == 0:
+                if arr+1 == arr1:
+                    return 0.5*p_MM
+                elif arr+2 == arr1:
+                    return 0.35*p_MM
+                elif arr+3 == arr1:
+                    return 0.15*p_MM
+                else:
+                    return 0
+            elif arr == 1:
+                if arr+1 == arr1:
+                    return 0.5*p_MM
+                elif arr+2 == arr1:
+                    return 0.5*p_MM
+                else:
+                    return 0
+            elif arr == 2:
+                if arr+1 == arr1:
+                    return p_MM
+                else:
+                    return 0
+            elif arr == 3:
+                if arr == arr1:
+                    return p_MM
+                else:
+                    return 0
         else:
             return 0
         
     elif a == 'GATHER':
-        # if p!=p1 or s!=s1 or h!=h1 or arr!=arr1:
-        #     return 0
         if p!=p1 or h!=h1 or arr!=arr1:
             return 0
-        if p == 'S' and m<=1:
-            if m+1 == m1:
-                return 0.75
-            elif m == m1:
-                return 0.25
+        if p == 'S':
+            if m+1 == m1 and m<=1:
+                return 0.75*p_MM
+            elif m == m1 and m<=1:
+                return 0.25*p_MM
+            elif m == m1 and m==2:
+                return p_MM
             else:
                 return 0
         else:
             return 0
     
-    elif(a == 'NONE'):
+    elif a == 'NONE':
         return 0
 
 r = []
 A = []
 temp_x = []
 policy = []
-actions_list = []
 col = 0
 flag = 0
 for p in all_pos:
@@ -322,7 +391,6 @@ for p in all_pos:
                 for h in range(0,max_health):
                     num_p = pos_map[p]
                     num_s = state_map[s]
-                    act = []
                     column = []
                     # column = [] ---> 
                     if( h == 0 ):
@@ -337,22 +405,15 @@ for p in all_pos:
                         A.append(column)
                         continue
                     for a in actions[p]:
-                        if(h == 0):
-                            pass
-                        elif a == 'SHOOT' and arr == 0:
+                        if a == 'SHOOT' and arr == 0:
                             continue
                         elif a == 'CRAFT' and (m == 0):
                             continue
-                        # noop condition
-                        if(not h):
-                            r.append(0)
-                            # if unsuccessful add -40 (doubt here i.e. how to add)
-                        else:
-                            r.append(action_reward[a])
+                        # if unsuccessful add -40 (doubt here i.e. how to add)
+                        r.append(STEP_COST)
                         col+=1
                         column.clear()
                         # column since later will take transpose to change to required A matrix
-                        act.append(a)
                         for p1 in all_pos:
                             for m1 in range(0,max_mat):
                                 for arr1 in range(0,max_arrow):
@@ -369,20 +430,21 @@ for p in all_pos:
                         # print(type)
                         column[tupletonum(pos_map[p],m,arr,state_map[s],h)] = -type
                         A.append(column)
-                    if(flag == 0):
-                        flag= 1
-                        for i in column:
-                            print(i)
-                    actions_list.append(act)
-
-# print("len r : " + str(len(r)))
-# print(r)
+                    # if(flag == 0):
+                    #     flag= 1
+                    #     for i in column:
+                    #         print(i)
 
 r = np.array(r)
 r.shape = (1,-1)
+
+for i in r.tolist():
+    print(i)
+
 A = np.array(A)
 # transpose of A
 A = np.transpose(A)
+
 alpha = [0 for i in range(0,total_states)]
 alpha[tupletonum(pos_map['W'],0,0,state_map['D'],0)] = 1
 alpha = np.array(alpha)
@@ -415,7 +477,8 @@ objective = cp.Maximize(cp.matmul(r,x))
 problem = cp.Problem(objective, constraints)
 solution = problem.solve()
 
-# print(problem.value)
+print(problem.status)
+print(problem.value)
 
 # for getting values of x
 
